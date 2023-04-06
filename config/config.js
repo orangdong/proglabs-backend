9bd482bd9d05e56e512eca7c734797b3320f1789
Aviel Naftali <dev.naftaliaviel@gmail.com>
2023-04-06 12:06:46 +0700

    check signature & token middleware

--- config/config.js
+++ config/config.js
@@ -7,6 +7,7 @@ dotenvExpand.expand(myEnv);
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || "0.0.0.0";
 const appKey = process.env.APP_KEY || "";
+const jwtSecret = process.env.JWT_SECRET || null;
 
 const mainDbConn = {
   url: `${process.env.DB_URL}`,
@@ -17,4 +18,5 @@ export default {
   host,
   mainDbConn,
   appKey,
+  jwtSecret,
 };
---
+++ src/routes/v1/middlewares/checkToken.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import config from "../../../../config/config.js";
+
+export default (req, res, next) => {
+  const token = req.headers["x-auth-token"];
+
+  if (!token) {
+    return res.status(401).json({
+      status: "error",
+      message: "Unauthorized, No token provided",
+    });
+  }
+
+  const validate = jwt.verify(token, config.jwtSecret);
+
+  if (!validate) {
+    return res.status(401).json({
+      status: "error",
+      message: "invalid token",
+    });
+  }
+  req.user = validate;
+
+  next();
+};
