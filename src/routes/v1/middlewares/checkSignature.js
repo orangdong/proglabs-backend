dd2209c69b4b921aaa24544a33fc41f1304cd460
Aviel Naftali <dev.naftaliaviel@gmail.com>
2023-04-08 17:23:54 +0700

    check signature middleware

--- src/routes/v1/middlewares/checkSignature.js
+++ src/routes/v1/middlewares/checkSignature.js
@@ -1,3 +1,34 @@
+import nacl from "tweetnacl";
+import b58 from "bs58";
+import { PublicKey } from "@solana/web3.js";
+
 export default (req, res, next) => {
+  const sigHeader = req.headers["x-auth-signature"];
+
+  if (!sigHeader) {
+    return res.status(401).json({
+      status: "error",
+      message: "Unauthorized, No signature provided",
+    });
+  }
+
+  const message = new TextEncoder().encode(
+    `Welcome to Proglabs,\nYour New Experience Learning Platform Partner!\n\nBefore continue using our services please click to sign in.\n\nNo password is required.\n\nYour authentication status will reset after 3 hour or after disconnecting your wallet.`
+  );
+  const { publicKey } = req.body;
+
+  const hasValidSig = nacl.sign.detached.verify(
+    message,
+    b58.decode(sigHeader),
+    new PublicKey(publicKey).toBytes()
+  );
+
+  if (!hasValidSig) {
+    return res.status(401).json({
+      status: "error",
+      message: "Invalid signature",
+    });
+  }
+
   next();
 };
